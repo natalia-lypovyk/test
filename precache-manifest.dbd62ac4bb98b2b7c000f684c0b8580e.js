ea773226b9e6eb5a7cc4d956689662db40e78b67
natalia-lypovyk <cardinnata@gmail.com>
2020-06-26 16:42:06 +0300

    Updates

---
+++ precache-manifest.dbd62ac4bb98b2b7c000f684c0b8580e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e97d12c5078edae80d81bc1d00be83",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b08528adfff8d02cfce",
+    "url": "./static/css/main.d57de78f.chunk.css"
+  },
+  {
+    "revision": "afa38025fb0dd4e7f53d",
+    "url": "./static/js/2.dbe3ea79.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "./static/js/2.dbe3ea79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b08528adfff8d02cfce",
+    "url": "./static/js/main.3ad32372.chunk.js"
+  },
+  {
+    "revision": "066e1fe302588d5b0de2",
+    "url": "./static/js/runtime-main.560b22e6.js"
+  },
+  {
+    "revision": "e3c80e2b2449d0162c762a2b5961b380",
+    "url": "./static/media/email.e3c80e2b.svg"
+  },
+  {
+    "revision": "47be60baa8a7281a95b735338d2d4b18",
+    "url": "./static/media/name.47be60ba.svg"
+  },
+  {
+    "revision": "787dbd1b14e7f3eedaa155c7326520bf",
+    "url": "./static/media/phone.787dbd1b.svg"
+  }
+]);
